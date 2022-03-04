5424067d4282a5f79ecfc29204e4a84d1ec64067
priyanka <priyanka3858@gmail.com>
2022-03-03 22:31:17 -0500

    vercel fix

--- main.py
+++ main.py
@@ -2,11 +2,7 @@ from flask import Flask, render_template
 import requests
 
 # Create a flask app
-app = Flask(
-  __name__,
-  template_folder='templates',
-  static_folder='static'
-)
+app = Flask(__name__)
 
 # Index page
 @app.route('/')
